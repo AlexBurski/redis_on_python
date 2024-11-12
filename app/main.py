c8366e067d7ceb37bd1163456e2c1b73d029dcf2
Alexander Burski <bssb.sbbs@mail.ru>
2024-11-11 19:59:09 -0500

    Add response to the PING command with hardcoded PONG

--- app/main.py
+++ app/main.py
@@ -8,8 +8,11 @@ def main():
     # Uncomment this to pass the first stage
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
-
+    client_socket, client_address = server_socket.accept()
+    response = "+PONG\r\n"
+    client_socket.sendall(response.encode())
+    client_socket.close()
+    server_socket.close()
 
 if __name__ == "__main__":
     main()
